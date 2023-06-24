b9c1eca7b8cba97a1f5a46fc73b9db3cd3cd209e
Kevin Ryano <kevinryano024@gmail.com>
2023-06-25 04:53:57 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import time
+import streamlit as st
+from streamlit_option_menu import option_menu
+from tools.gridding import grid_vel, create_vts
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Tool Bar",
+        options=["Home", "Gridding", "Stacking", "Inversion"],
+        menu_icon="cast",
+        default_index=0
+    )
+
+if selected == "Home":
+    st.title("Seismic Tomography Toolkitgit")
+    st.write("By Kevin Ryano (@kevinryano)")
+    st.markdown("---")
+
+if selected == "Gridding":
+    st.title("Gridding")
+    st.write("Tomogram gridding tools")
+    st.markdown("---")
+
+    delta = st.number_input("Input Delta XYZ", min_value=10, max_value=100, step=5)
+    interp_method = st.radio("Input Interpolation Method", options=['linear', 'rbf'])
+
+    nama_file_grid = st.text_input("File Output Name")
+    veldat = st.file_uploader("Upload Velocity Data", type=["csv"])
+    gridBtn = st.button("Start Gridding")
+
+    if gridBtn:
+        t0 = time.perf_counter()
+        x, y, z, vp, vs, ps = grid_vel(veldat, delta=int(delta), interp_method=interp_method)
+        create_vts(x, y, z, vp, vs, ps, nama_file=nama_file_grid)
+        t1 = time.perf_counter()
+        st.markdown("***")
+        st.text(f"========== {nama_file_grid}.vts Created ==========\n")
+        st.text(f"> Interpolation Method \t: {interp_method}")
+        st.text(f"> Runtime \t\t: {t1-t0:.2f} s")
+
+if selected == "Stacking":
+    st.title("Stacking App")
+    st.markdown("---")
+    st.write("Coming Soon...")
+
+if selected == "Inversion":
+    st.title("Inversion App")
+    st.markdown("---")
+    st.write("Coming Soon..")
---
+++ tools/gridding.py
@@ -0,0 +1,38 @@
+import os
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pyevtk.hl import pointsToVTK, gridToVTK
+from scipy.interpolate import griddata, Rbf
+
+def grid_vel(data, delta=10, interp_method="rbf"):
+    data = pd.read_csv(data)
+    # Reformat data to numpy array
+    x = data['X'].values
+    y = data['Y'].values
+    z = data['Z'].values
+    vp = data['Vp'].values
+    vs = data['Vs'].values
+    vp_vs = data['Vp/Vs'].values
+
+    # Create spatial grid
+    grid_x, grid_y, grid_z = np.meshgrid(np.arange(min(x), max(x)+delta, delta),
+                                         np.arange(min(y), max(y)+delta, delta),
+                                         np.arange(min(z), max(z)+delta, delta))
+
+    # Interpolate Vp, Vs, Vp/Vs
+    if interp_method == "linear":
+        grid_vp = griddata((x, y, z), vp, (grid_x, grid_y, grid_z), method='linear')
+        grid_vs = griddata((x, y, z), vs, (grid_x, grid_y, grid_z), method='linear')
+        grid_ps = griddata((x, y, z), vp_vs, (grid_x, grid_y, grid_z), method='linear')
+
+    if interp_method == "rbf":
+        grid_vp = Rbf(x, y, z, vp, function="thin_plate")(grid_x, grid_y, grid_z)
+        grid_vs = Rbf(x, y, z, vs, function="thin_plate")(grid_x, grid_y, grid_z)
+        grid_ps = Rbf(x, y, z, vp_vs, function="thin_plate")(grid_x, grid_y, grid_z)
+    return grid_x, grid_y, grid_z, grid_vp, grid_vs, grid_ps
+
+def create_vts(x, y, z, vp, vs, ps, nama_file="tomogram"):
+    os.makedirs("Hasil Gridding", exist_ok=True)
+    gridToVTK(f"./Hasil Gridding/{nama_file}", x, y, z, pointData={"Vp":vp, "Vs":vs, "Vp/Vs":ps})
